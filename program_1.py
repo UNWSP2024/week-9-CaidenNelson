aa13fa79caa8079d22feb37d2c23e78599c1a9f9
CaidenNelson <cwnelson4@students.unwsp.edu>
2024-11-01 18:57:48 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -2,7 +2,9 @@
 # Assume a file containing a series of names (as strings) is named names.txt 
 # (Use the included example file names.txt) and exists on the computer's disk.
 # Write a program that displays the number of names that are stored in the file.
-
+#Programmer = caiden
+#Date = 10.28.24
+#Title = Number of Names
 def count_file_lines():
     ######################
     # Add your code here #
